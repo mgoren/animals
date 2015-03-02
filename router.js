fbd9c88d0c447e28cc0ed0569e62ce8521000529
Lauren Marcus and Mike Goren <lauren.e.marcus@gmail.com>
2015-03-02 11:27:20 -0800

    Allow the user to access four sections of the site from the  carousel and images.

---
+++ router.js
@@ -0,0 +1,7 @@
+app.Router.map(function() {
+  this.resource('home', {path: '/'});
+  this.resource('gorilla');
+  this.resource('lion');
+  this.resource('lizard');
+  this.resource('warthog');
+});

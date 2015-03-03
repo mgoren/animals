236248f75f73bc14bead0fafc488c2e097da6c68
Lauren Marcus and Mike Goren <lauren.e.marcus@gmail.com>
2015-03-02 16:17:34 -0800

    add post page

--- controllers/posts.js
+++ controllers/posts.js
@@ -6,9 +6,11 @@ app.PostsController = Ember.Controller.extend({
   actions: {
     nextPost: function() {
       this.set('postID', this.postID+1);
-      if(this.postID>posts.length - 1) {
-        this.set('postID', 0);
-      }
+      if(this.postID>posts.length - 1) {this.set('postID', 0);}
+    },
+    previousPost: function() {
+      this.set('postID', this.postID-1);
+      if(this.postID < 0) {this.set('postID', posts.length -1);}
     }
   }
 });
--- router.js
+++ router.js
@@ -1,5 +1,6 @@
 app.Router.map(function() {
   this.resource('posts', {path: '/'});
+  this.resource('post', {path: '/post/:post_id'});
   this.resource('gorilla');
   this.resource('lion');
   this.resource('lizard');
